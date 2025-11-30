572a7035dff2b84cfa24d4d5c8b703778c46e5df
Markus Winkler <winkler.wm@gmail.com>
2025-11-30 12:16:22 +0100

    renamed container and image

--- create-docker-image.sh
+++ create-docker-image.sh
@@ -1,2 +1,2 @@
-docker build -t my-mcp-server .
-docker run -d --name my-mcp-server --network my-local-net -p 7999:7999 my-mcp-server
+docker build -t winklermw-mz/mymcp .
+docker run -d --name MyMCP --network my-local-net -p 7999:7999 winklermw-mz/mymcp
